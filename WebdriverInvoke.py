be2e1fb0926d14873bf281cb1cb409a9c7294728
flash958 <saxena958@gmail.com>
2024-05-26 12:36:15 +0530

    first commit

---
+++ WebdriverInvoke.py
@@ -0,0 +1,26 @@
+import time
+
+import options
+import selenium
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+
+options=Options()
+
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()),options=options)
+
+driver.get("https://www.google.com/")
+time.sleep(5)
+driver.find_element(By.XPATH,value="//textarea[@id='APjFqb']").send_keys("Python")
+time.sleep(5)
+print(driver.title)
+
+
+
+
+
+
+
