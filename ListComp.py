d494fa14af27242e6846dd04480898777a61933d
flash958 <saxena958@gmail.com>
2024-05-26 22:42:09 +0530

    Updated List Comprehension

--- ListComp.py
+++ ListComp.py
@@ -1,3 +1,9 @@
+# List Comprehension Example 1
 l=[1,2,3,4,5,6,7,8]
 result = [i**2 for i in l if i%2==0 ]
 print(result)
+
+# List comprehension Example 2
+l=[1,2,3,4,5,6,7,8,9,10]
+table = [i*2 for i in l]
+print(table)
